87ce5cdcf50d4d12bd976b4ca4ae1f980fc9b787
김원혁 <kwh102455@gmail.com>
2025-10-26 14:26:08 +0900

    feat(ExceptionMessage): 예외 메시지 enum 클래스 구현

---
+++ src/main/java/racingcar/exception/ExceptionMessage.java
@@ -0,0 +1,19 @@
+package racingcar.exception;
+
+public enum ExceptionMessage {
+  EMPTY_NAME("자동차 이름이 비어있습니다."),
+  NAME_TOO_LONG("자동차 이름은 5자 이하여야 합니다."),
+  DUPLICATE_NAME("자동차 이름이 중복되어있습니다."),
+  IS_NOT_INTEGER("입력받은 숫자값이 정수가 아닙니다."),
+  IS_NOT_POSITIVE_NUM("입력받은 숫자값이 양수가 아닙니다.");
+
+  private String message;
+
+  private ExceptionMessage(String message){
+    this.message = message;
+  }
+
+  public String getMessage(){
+    return message;
+  }
+}
