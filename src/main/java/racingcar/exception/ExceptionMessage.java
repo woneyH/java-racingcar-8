53dbe5837e4241cbe96387d9e3c9b09458876d84
김원혁 <kwh102455@gmail.com>
2025-10-26 15:47:11 +0900

    refactor(ExceptionMessage): 예외 메시지 수정

--- src/main/java/racingcar/exception/ExceptionMessage.java
+++ src/main/java/racingcar/exception/ExceptionMessage.java
@@ -5,7 +5,7 @@ public enum ExceptionMessage {
   NAME_TOO_LONG("자동차 이름은 5자 이하여야 합니다."),
   DUPLICATE_NAME("자동차 이름이 중복되어있습니다."),
   IS_NOT_INTEGER("입력받은 숫자값이 정수가 아닙니다."),
-  IS_NOT_POSITIVE_NUM("입력받은 숫자값이 양수가 아닙니다.");
+  IS_NOT_POSITIVE_NUM("입력받은 숫자값이 양수가 아닙니다. 또는 0입니다.");
 
   private String message;
 
